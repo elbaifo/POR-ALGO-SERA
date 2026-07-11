55ee8f7e4f103a7b6978e35dc5194fe1e0938fc7
elbaifo <alvaroo.fernandezz7@gmail.com>
2026-07-11 18:11:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("POR ALGO SERÁ");
+const popup = document.getElementById("welcome-popup");
+const enterButton = document.getElementById("enter-button");
+
+if (localStorage.getItem("welcomeSeen")) {
+    popup.style.display = "none";
+}
+
+enterButton.addEventListener("click", () => {
+
+    popup.classList.add("hidden");
+
+    localStorage.setItem("welcomeSeen", "true");
+
+    setTimeout(() => {
+        popup.style.display = "none";
+    }, 500);
+
+});
